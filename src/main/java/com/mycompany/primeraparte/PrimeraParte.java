0411bf208c893d70c80f3fbefec25f93204aa26c
gmateoc <pas_ul15@hotmail.com>
2022-07-06 16:58:18 -0500

    se agrego contenido

---
+++ src/main/java/com/mycompany/primeraparte/PrimeraParte.java
@@ -0,0 +1,12 @@
+package com.mycompany.primeraparte;
+
+public class PrimeraParte {
+
+    public static void main(String[] args) {
+        suma(1,2,4);
+    }
+    public static void suma(int a,int b, int c){
+        int resultado = a+b+c;
+        System.out.println(resultado);
+    }
+}
